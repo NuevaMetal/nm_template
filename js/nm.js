eb780733320d9f07af64d7a9649985cf859968b0
Chemaclass <chemaclass@outlook.es>
2014-07-28 23:07:13 +0200

    Añadimos js para poner el menu-principal en top cuando hagamos scroll

--- js/nm.js
+++ js/nm.js
@@ -1,17 +1,24 @@
 /*
  Autor: José María Valera Reales <@Chemaclass>
-*/
+ */
 
 function subheaderLoginFormToogle() {
 	$('#subheader #loginform .ayuda').slideToggle();
 }
 
-$( document ).ready(function() {
+$(document).ready(function() {
 	subheaderLoginFormToogle();
-	$('#subheader #loginform ').click(function(){
+	$('#subheader #loginform ').click(function() {
 		subheaderLoginFormToogle();
 		console.log("clicaste");
 	});
 });
 
-
+$(window).scroll(function() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 200) {
+		$(".navbar-principal").addClass("navbar-fixed-top");
+	} else {
+		$(".navbar-principal").removeClass("navbar-fixed-top");
+	}
+});
