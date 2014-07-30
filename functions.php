c993ad692f7dbee20753a6f7b83f3197d1b01ea2
Chemaclass <chemaclass@outlook.es>
2014-07-30 03:41:34 +0200

    Añado post-thumbnails en funcitons para poder añadir miniaturas a los posts

--- functions.php
+++ functions.php
@@ -1,3 +1,8 @@
 <?php
 require_once (dirname(__FILE__) . '/lib/chester/require.php');
 require_once (dirname(__FILE__) . '/lib/chemaclass/require.php');
+
+add_theme_support('post-thumbnails', array(
+	'post',
+	'page'
+));
