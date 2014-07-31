0ded67d52a1a65017d395800b09667c78b3ffedb
Chemaclass <chemaclass@outlook.es>
2014-07-31 22:08:50 +0200

    Añado movimiento por js a los titulos en home

--- js/nm.js
+++ js/nm.js
@@ -30,3 +30,50 @@ function scrollOff() {
 	$(".navbar-login").removeClass("hidden");
 	$("#content").removeClass("aumentar-padding-top-content");
 }
+
+/**
+ * Mover texto a la izquierda
+ * 
+ * @param elem
+ *            Elemento contenedor a mover
+ */
+function moverTextoIzquierda(elem) {
+	var text = $(elem).children('a').text();
+	function getTiempo(len) {
+		if (len < 40)
+			return len * 0.4;
+		else if (len < 50)
+			return len * 2.2;
+		else if (len < 60)
+			return len * 3;
+		else if (len < 100)
+			return len * 4;
+		return len;
+	}
+	var c = getTiempo(text.length);
+	var w = $(window).width();
+	// Sólo animar si no está siendo animado ya. Para evitar sobrecarga
+	if (!$(elem).is(':animated') && (w > COL.SM ||  w < 500)) {
+		$(elem).animate({
+			marginLeft : "-=" + c
+		}, 1000, function() {
+			setTimeout(function() {
+				$(elem).removeAttr('style');
+			}, 600);
+		});
+	}
+}
+
+var COL = {
+	SM : 768,
+	MD : 992,
+	LG : 1200
+};
+
+$(document).ready(function() {
+	$('.post-title').hover(function() {
+		moverTextoIzquierda(this);
+	}, function() {
+		// $(this).removeAttr('style');
+	});
+});
