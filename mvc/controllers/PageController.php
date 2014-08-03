39ae18a38386b6f80d620c0e436ecd654dd30f27
Chemaclass <chemaclass@outlook.es>
2014-08-03 13:45:53 +0200

    Comenzando a dividir la home por secciones

--- mvc/controllers/PageController.php
+++ mvc/controllers/PageController.php
@@ -38,6 +38,44 @@ class PageController extends BaseController {
 	}
 
 	/**
+	 * home.php
+	 */
+	public function getHomePorSecciones() {
+		$catBandas = get_cat_ID('bandas');
+		$catVideos = get_cat_ID('videos');
+		$catEntrevistas = get_cat_ID('entrevistas');
+
+		$bandas = self::_getPostsByCategory($catBandas, 4);
+		$videos = self::_getPostsByCategory($catVideos, 4);
+		$entrevistas = self::_getPostsByCategory($catEntrevistas, 4);
+
+		$posts = array_merge($bandas, $videos, $entrevistas);
+
+		$content = $this->_renderHome([
+			'posts' => $posts
+		]);
+		return $this->_renderPageBase([
+			'content' => $content
+		]);
+	}
+
+	/**
+	 * Devuelve un número determinado de posts en base al ID de su categoría
+	 *
+	 * @param number $catId
+	 *        ID de la categoría
+	 * @param number $max
+	 *        número máximo de posts a devolver
+	 * @return multitype:
+	 */
+	private static function _getPostsByCategory($catId, $max = 4) {
+		$moreQuerySettings = [
+			'cat' => "$catId"
+		];
+		return ChesterWPCoreDataHelpers::getPosts(false, 'post', $max, [], false, $moreQuerySettings);
+	}
+
+	/**
 	 * category.php
 	 */
 	public function getCategory() {
