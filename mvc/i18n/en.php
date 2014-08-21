5c3cba63f21af0e59e72b8813444119649345793
Chemaclass <chemaclass@outlook.es>
2014-08-21 21:42:50 +0200

    Añado ficheros de idiomas básicos en, es

--- mvc/i18n/en.php
+++ mvc/i18n/en.php
@@ -1,5 +1,54 @@
 <?php
 return [
-	'bienvenido' => 'Wellcome',
-	'hola' => 'Hello!'
+	'acceder' => 'login',
+
+	'bandas' => 'bands',
+	'bienvenido' => 'welcome',
+	'biografia' => 'biography',
+	'buscar' => 'search',
+
+	'cerrar_sesion' => 'logout',
+	'conciertos' => 'concerts',
+	'contrasena' => 'password',
+	'contrasena_perdida' => 'password?',
+	'criticas' => 'reviews',
+	'cronicas' => 'chronics',
+
+	'de' => 'of',
+
+	'hola' => 'hello',
+
+	'editar_perfil' => 'edit profile',
+	'entrada' => 'post',
+	'entradas' => 'posts',
+	'entradas_de' => '{{nombre}}\'s posts',
+	'entrevistas' => 'interviews',
+	'escritorio' => 'dashboard',
+	'escribir' => 'write',
+
+	'mostrar_mas' => 'show more',
+
+	'noticias' => 'news',
+	'nueva_entrada' => 'new post',
+
+	'ok' => 'ok',
+
+	'pagina_anterior' => 'previous page',
+	'pagina_siguiente' => 'next page',
+	'perfil' => 'profile',
+	'por' => 'by',
+
+	'recuerdame' => 'remember me',
+	'registrarse' => 'Sign in',
+
+	'salir' => 'exit',
+	'secciones' => 'sections',
+	'sitios_de_interes' => 'interest sites',
+
+	'usuario' => 'user',
+	'unete_y_colabora' => 'join and collaborate',
+
+	'ver_perfil' => 'see profile',
+	'videos' => 'videos',
+	'volver_arriba' => 'back to top'
 ];
--- mvc/i18n/es.php
+++ mvc/i18n/es.php
@@ -1,5 +1,54 @@
 <?php
 return [
-	'bienvenido' => 'Bienvenido',
-	'hola' => '¡Hola!'
+	'acceder' => 'acceder',
+
+	'bandas' => 'bandas',
+	'bienvenido' => 'bienvenido',
+	'biografia' => 'biografía',
+	'buscar' => 'buscar',
+
+	'cerrar_sesion' => 'cerrar Sesión',
+	'conciertos' => 'conciertos',
+	'contrasena' => 'contraseña',
+	'contrasena_perdida' => '¿Contraseña?',
+	'criticas' => 'críticas',
+	'cronicas' => 'crónicas',
+
+	'de' => 'de',
+
+	'hola' => 'hola',
+
+	'editar_perfil' => 'editar perfil',
+	'entrada' => 'entrada',
+	'entradas' => 'entradas',
+	'entradas_de' => 'entradas de {{nombre}}',
+	'entrevistas' => 'entrevistas',
+	'escritorio' => 'escritorio',
+	'escribir' => 'escribir',
+
+	'mostrar_mas' => 'mostrar más',
+
+	'noticias' => 'noticias',
+	'nueva_entrada' => 'nueva entrada',
+
+	'ok' => 'ok',
+
+	'pagina_anterior' => 'página anterior',
+	'pagina_siguiente' => 'página siguiente',
+	'perfil' => 'perfil',
+	'por' => 'por',
+
+	'recuerdame' => 'recuérdame',
+	'registrarse' => 'registrarse',
+
+	'salir' => 'salir',
+	'secciones' => 'secciones',
+	'sitios_de_interes' => 'sitios de interés',
+
+	'usuario' => 'usuario',
+	'unete_y_colabora' => 'únete y colabora',
+
+	'ver_perfil' => 'ver perfil',
+	'videos' => 'vídeos',
+	'volver_arriba' => 'volver arriba'
 ];
