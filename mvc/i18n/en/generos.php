970d3d4adf02fca2b65ffc2dac3cfb8b8748f9d8
Chemaclass <chemaclass@outlook.es>
2014-08-22 19:31:19 +0200

    Añado fichero de idiomas de generos [en,es]

---
+++ mvc/i18n/en/generos.php
@@ -0,0 +1,32 @@
+<?php
+return [
+
+	/**
+	 * Metal
+	 */
+	'alternative-metal' => 'Alternative metal',
+	'black-metal' => 'Black metal',
+	'death-metal' => 'Death metal',
+	'melodic-death' => 'Melodic death',
+	'folk-metal' => 'Folk metal',
+	'gothic-metal-2' => 'Gothic metal',
+	'groove-metal' => 'Groove metal',
+	'heavy-metal' => 'Heavy metal',
+	'industrial-metal' => 'Industrial metal',
+	'melodic-metal' => 'Melodic metal',
+	'nu-metal' => 'Nu metal',
+	'power-meta' => 'Power metal',
+	'progressive-metal' => 'Progressive metal',
+	'thrash-metal' => 'Thrash metal',
+	'technical-death' => 'Technical death',
+
+	/**
+	 * Core
+	 */
+	'deathcore' => 'Deathcore',
+	'djent' => 'Djent',
+	'hardcore' => 'Hardcore',
+	'metalcore' => 'Metalcore',
+	'post-hardcore' => 'Post hardcore',
+	'trancecore' => 'Trancecore'
+];
---
+++ mvc/i18n/es/generos.php
@@ -0,0 +1,32 @@
+<?php
+return [
+
+	/**
+	 * Metal
+	 */
+	'alternative-metal' => 'Metal alternativo',
+	'black-metal' => 'Black metal',
+	'death-metal' => 'Death metal',
+	'melodic-death' => 'Death metal melódico',
+	'folk-metal' => 'Folk Metal',
+	'gothic-metal-2' => 'Gothic metal',
+	'groove-metal' => 'Groove metal',
+	'heavy-metal' => 'Heavy metal',
+	'industrial-metal' => 'Metal industrial',
+	'melodic-metal' => 'Metal melódico',
+	'nu-metal' => 'Nu Metal',
+	'power-meta' => 'Power Metal',
+	'progressive-metal' => 'Metal progresivo',
+	'thrash-metal' => 'Thrash Metal',
+	'technical-death' => 'Death metal técnico',
+
+	/**
+	 * Core
+	 */
+	'deathcore' => 'Deathcore',
+	'djent' => 'Djent',
+	'hardcore' => 'Hardcore',
+	'metalcore' => 'Metalcore',
+	'post-hardcore' => 'Post hardcore',
+	'trancecore' => 'Trancecore'
+];
