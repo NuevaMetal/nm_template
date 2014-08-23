3721fd63d0b0ea700c0a805599d78eb964f74e68
Chemaclass <chemaclass@outlook.es>
2014-08-23 14:30:33 +0200

    Añado i18n al sidebar

---
+++ mvc/i18n/en/sidebar.php
@@ -0,0 +1,19 @@
+<?php
+return [
+	'bienvenido' => 'Welcome!',
+
+	'empieza_a_publicar_largo' => 'Post your metal music!',
+	'empieza_a_publicar' => 'Post now!',
+	'entradas_similares' => 'Similar entries',
+
+	'unete' => 'Join now to our metal community!!',
+
+	'si_no_quieres_aparecer' => 'If you wont be in NuevaMetal, please, send us a mail to
+		<span class="hidden-sm">nuevametal[@]outlook.com</span>
+		<span class="visible-sm"><small>nuevametal[@]outlook.com</small></span>
+		and we delete your content.',
+
+	'te_gusta_lo_que_ves' => 'Do you like it?
+		Do you have a metal band and (you want be know/you want appear in NuevaMetal)?
+		Would you like (to) post your entries?'
+];
---
+++ mvc/i18n/es/sidebar.php
@@ -0,0 +1,20 @@
+<?php
+return [
+	'bienvenido' => '¡Bienvenido!',
+
+	'empieza_a_publicar_largo' => 'Empieza a publicar tu música metal',
+	'empieza_a_publicar' => '¡Empieza a publicar!',
+	'entradas_similares' => 'Entradas similares',
+
+	'unete' => '¡Únete a la comundidad nuevametalera!',
+
+	'si_no_quieres_aparecer' => 'Si no quieres aparecer en NuevaMetal envíanos un correo a
+		<span class="hidden-sm">nuevametal[@]outlook.com</span>
+		<span class="visible-sm"><small>nuevametal[@]outlook.com</small></span>
+		y borraremos el contenido relacionado con tu banda.',
+
+	'te_gusta_lo_que_ves' => '¿Te gusta lo que ves?<br>
+		¿Tienes una banda y quieres difusión?<br>
+		¿Te gustaría publicar tus bandas preferidas? <br>
+		¿Te gusta escribir críticas de los nuevos álbumes?'
+];
