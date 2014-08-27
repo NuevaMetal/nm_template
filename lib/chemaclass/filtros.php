0f58877b8e06d4461b485ec688465450c5b3e890
Chemaclass <chemaclass@outlook.es>
2014-08-27 21:43:39 +0200

    Implemento funcionalidad html::quitarAtributos. Utilidad Html para quitar attr a las etiquetas que se le indiquen, o vicebersa.

--- lib/chemaclass/filtros.php
+++ lib/chemaclass/filtros.php
@@ -29,8 +29,10 @@ function bootstrap3_comment_form_fields( $fields ) {
 add_filter('the_content', function ($content) {
 	global $post;
 	$alto = (in_category('videos', $post)) ? 360 : 300;
-	$pattern = '/<iframe width="[0-9]+" height="[0-9]+"/i';
+	$pattern = '/<iframe width="[0-9]+%?" height="[0-9]+"/i';
 	$replacement = '<iframe width="100%" height="' . $alto . '"';
-	$content = preg_replace($pattern, $replacement, $content);
+
+ 	$content = preg_replace($pattern, $replacement, $content);
+ 	$content = Html::quitarAtributos($content,['iframe','img']);
 	return $content;
 });
--- lib/chemaclass/html.php
+++ lib/chemaclass/html.php
@@ -2,35 +2,37 @@
 /**
  * Clase con recursos para el HTML
  *
- * @author chemaclass
+ * @author José María Valera Reales <@Chemaclass>
  *
  */
 class Html {
 
 	/**
-	 * Quitar atributos a las etiquetas html que se encuentre.
-	 * Todos los atributos por default, pero se le pueden especificar
-	 * cuales, pasándoles su nombre en un array como segundo parámetro
+	 * Quitar atributos a las etiquetas html que se encuentre, o menos las que se encuentre.
+	 * Por defecto las etiquetas indicadas (2º param) serán de las que no se borrarán sus attr.
 	 *
 	 * @param string $html
 	 *        HTML a buscar
 	 * @param string $attr
-	 *        Lista con los nombres de los attr a eliminar
+	 *        Lista con los nombres de las etiquetas que queremos encontrar
+	 * @param boolean $aBorrar
+	 *        true indica que se deben borrar los attr de las etiquetas indicadas
+	 *        y false se indica que se tienen que borrar todos los attr de las
+	 *        etiquetas menos las indicadas
 	 * @return html sin los attr especificados en sus etiquetas
 	 */
-	public static function quitarAtributos($html, $attr = null) {
-		if ($attr == null) {
-			Utils::debug("attr == null");
+	public static function quitarAtributos($html, $attr = array(), $aBorrar = false) {
+		if (empty($attr)) {
 			return preg_replace("/<([a-z][a-z0-9]*)[^>]*?(\/?)>/i", '<$1$2>', $html);
 		}
-		Utils::debug("attr != null");
 		array_walk($attr, function (&$item) {
-			$item .= '="([a-z][a-z0-9]*)"';
+			$item = "\b$item\b";
 		});
-		$attrStr = '[' . implode('|', $attr) . ']';
-		// $pattern = "/<([a-z][a-z0-9]*)(?=$attrStr)[^>]*?(\/?)>/i"; // ?
-		// dd($pattern);
-		// TODO: Falta implementar, poder elegir qué attr eliminar
+		$attrStr = implode('|', $attr);
+		$accion = ($aBorrar) ? '=' : '!';
+		$pattern = '~<(?' . $accion . '[' . $attrStr . '])([a-z][a-z0-9]*)[^>]*(/?)>~is';
+		// 	dd($pattern); // '~<(?![\bimg\b|\biframe\b])([a-z][a-z0-9]*)[^>]*(/?)>~is'
+		$html = preg_replace($pattern, '<$1$2>', $html);
 		return $html;
 	}
 
