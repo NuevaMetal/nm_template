64a752441795e9ecff65be5fe953e022da5ee84c
Chemaclass <chemaclass@outlook.es>
2014-09-05 22:44:39 +0200

    Vuelvo a habilitar los btn de registro

--- lib/chemaclass/require.php
+++ lib/chemaclass/require.php
@@ -2,8 +2,7 @@
 
 $path = dirname(__FILE__);
 
-//require_once( $path . '/utils.php');
+require_once( $path . '/acciones.php');
 require_once( $path . '/filtros.php');
 require_once( $path . '/html.php');
-//require_once( $path . '/admin_gallery.php');
-//require_once( $path . '/admin_bandas.php');
+require_once( $path . '/../PHPMailer/PHPMailerAutoload.php');
---
+++ mvc/lib/Correo.php
@@ -0,0 +1,56 @@
+<?php
+class Correo {
+
+	/**
+	 * Devuelve una nueva instancia del PHPMailer
+	 *
+	 * @param string|array $email
+	 * @return PHPMailer
+	 */
+	private function getNuevoMailer($email = '') {
+		$mail = new PHPMailer();
+		$mail->isSMTP(); // Set mailer to use SMTP
+		$mail->Host = 'smtp.mandrillapp.com'; // Specify main and backup SMTP servers
+		$mail->SMTPAuth = true; // Enable SMTP authentication
+		$mail->Username = 'chemaclass@outlook.es'; // SMTP username
+		$mail->Password = '33ct-NhoNPVIa3Ay92_E2g'; // SMTP password
+		$mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
+		$mail->Port = 587; // TCP port to connect to
+		$mail->CharSet = 'utf-8';
+
+		$mail->From = 'nuevametal@outlook.com';
+		$mail->FromName = 'NuevaMetal';
+
+		if (is_string($email)) {
+			$mail->addAddress($email);
+		} else if (is_array($email)) {
+			foreach ($email as $e) {
+				$mail->addAddress($e);
+			}
+		}
+		return $mail;
+	}
+
+	/**
+	 * Enviar un correo genérico
+	 *
+	 * @param array|string $email
+	 *        Destino
+	 * @param string $subject
+	 *        Asunto
+	 * @param string $bodyHtml
+	 * @param string $bodyHtmlAlt
+	 * @return boolean
+	 */
+	public static function enviarCorreoGenerico($email, $subject, $bodyHtml, $bodyHtmlAlt = '') {
+		$mail = self::getNuevoMailer($email);
+
+		$mail->WordWrap = 50; // Set word wrap to 50 characters
+		$mail->isHTML(true); // Set email format to HTML
+		$mail->Subject = $subject;
+		$mail->Body = $bodyHtml;
+		$mail->AltBody = $bodyHtmlAlt;
+		return $mail->send();
+	}
+
+}
