a4099e34e7a4209ae1a310e1bf495a7834b86dd4
Chemaclass <chemaclass@outlook.es>
2014-09-05 22:44:19 +0200

    Añado i18n emails

---
+++ mvc/i18n/en/emails.php
@@ -0,0 +1,16 @@
+<?php
+return [
+	'nuevo_user' => 'Hi there,
+		<br>
+		Welcome to NuevaMetal! Heres how to
+			<a href="http://dev.nuevametal.local/wp-login.php"> log in</a>:<br>
+		<b>Username</b>: :user_login <br>
+		<b>Password</b>: :user_pass <br>
+		<br>
+		If you have any problems, please contact me at :admin_email <br>
+		See you!',
+	'aviso_admin_nuevo_user' => '
+		New user registered in :blogname <br>
+		<b>Username</b>: :user_login <br>
+		<b>E-mail</b>: :user_email <br>'
+];
---
+++ mvc/i18n/es/emails.php
@@ -0,0 +1,16 @@
+<?php
+return [
+	'nuevo_user' => '¡Ey bro!,
+		<br>
+		¡Bienvenido a NuevaMetal! Aquí tienes cómo hacer:
+			<a href="http://dev.nuevametal.local/wp-login.php">login</a>:<br>
+		<b>Nombre de usuario</b>: :user_login <br>
+		<b>Contraseña</b>: :user_pass <br>
+		<br>
+		Si tienes algún problema, por favor contáctame :admin_email <br>
+		¡Nos vemos!',
+	'aviso_admin_nuevo_user' => '
+		Nuevo usuario registrado en :blogname <br>
+		<b>Nombre de usuario</b>: :user_login <br>
+		<b>E-mail</b>: :user_email <br>'
+];
