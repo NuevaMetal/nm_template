79cf69989a0b2c8c508a9ebf5da0be9568a9766e
Chemaclass <chemaclass@outlook.es>
2014-09-18 14:31:02 +0200

    Cambio la migración 4 por modifico meta key redes

---
+++ mvc/migrations/M_04_modifico_meta_key_redes.sql
@@ -0,0 +1,3 @@
+UPDATE wp_usermeta SET meta_key = 'tw_txt' WHERE meta_key =  'user_tw_txt';
+UPDATE wp_usermeta SET meta_key = 'fb_txt' WHERE meta_key =  'user_fb_txt';
+UPDATE wp_usermeta SET meta_key = 'gp_txt' WHERE meta_key =  'user_gp_txt';
