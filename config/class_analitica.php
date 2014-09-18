2f360087c7732ace48dfe1662991307b49041c00
Chemaclass <chemaclass@outlook.es>
2014-09-18 14:58:55 +0200

    Comento/Quito el crear nueva analitica cada vez que se carga la web. Con la action 'wp'

--- config/class_analitica.php
+++ config/class_analitica.php
@@ -44,14 +44,14 @@ function analitica_index() {
 }
 
 add_action('wp', function () {
-	try {
-		//Sólo guardar la analítica si no es de un bot o un feed
-		$user_agent = $_SERVER ['HTTP_USER_AGENT'];
-		if (!strpos($user_agent, 'bot') && !strpos($user_agent, 'feed')) {
-			$analitica = new Analitica();
-			$analitica->save();
-		}
-	} catch (Exception $e) {
-		Utils::debug('No se pudo guardar la Analitica ?');
-	}
+// 	try {
+// 		//Sólo guardar la analítica si no es de un bot o un feed
+// 		$user_agent = $_SERVER ['HTTP_USER_AGENT'];
+// 		if (!strpos($user_agent, 'bot') && !strpos($user_agent, 'feed')) {
+// 			$analitica = new Analitica();
+// 			$analitica->save();
+// 		}
+// 	} catch (Exception $e) {
+// 		Utils::debug('No se pudo guardar la Analitica ?');
+// 	}
 });
