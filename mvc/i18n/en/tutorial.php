000063c5b0bc3aaa94840b09ca700fc620cdafe4
Chemaclass <chemaclass@outlook.es>
2014-09-22 00:11:40 +0200

    Añadir acepto terminos y condiciones para pulsar el botón de solicitud para colaborar

--- mvc/i18n/en/tutorial.php
+++ mvc/i18n/en/tutorial.php
@@ -1,5 +1,9 @@
 <?php
 return [
+	'aceptas_terminos_condiciones' => 'If you want to create your own content on the
+			NuevaMetal web app you must agree to the <a href="/legal" target="_blank">terms and conditions</a>
+			of use',
+
 	'haz_que_tu_banda' => 'Make your band to appear in our sections',
 
 	'publica_tus_entradas' => 'Publish your posts',
--- mvc/i18n/es/tutorial.php
+++ mvc/i18n/es/tutorial.php
@@ -1,5 +1,9 @@
 <?php
 return [
+	'aceptas_terminos_condiciones' => 'Si solicitas crear tu contenido en la web aceptas
+			los <a href="/legal" target="_blank">términos
+			y condiciones</a> de uso de la aplicación web NuevaMetal',
+
 	'haz_que_tu_banda' => 'Haz que tu banda aparezca en nuestras secciones',
 
 	'publica_tus_entradas' => 'Publica tus entradas',
