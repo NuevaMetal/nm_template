bbbb8e2c17d5d7ca162c9e114fd1d8661b2fed05
Chemaclass <chemaclass@outlook.es>
2014-09-21 07:11:25 +0200

    Cambio de post a posts. Quito saludo de María en el tutorial y pongo enlace a redes

--- mvc/i18n/en/tutorial.php
+++ mvc/i18n/en/tutorial.php
@@ -2,6 +2,6 @@
 return [
 	'haz_que_tu_banda' => 'Make your band to appear in our sections',
 
-	'publica_tus_entradas' => 'Publish your post',
+	'publica_tus_entradas' => 'Publish your posts',
 	'pulsa_aqui_para_ser_colaborador' => 'Click here to become a contributor'
 ];
