5aa35e96a32aaf07ccc479e5331b7a17b4cdc2d3
Chemaclass <chemaclass@outlook.es>
2014-10-01 22:26:12 +0200

    mend
    Corrijo bug del captcha

--- mvc/lib/Acciones.php
+++ mvc/lib/Acciones.php
@@ -356,10 +356,7 @@ class Acciones {
 		//2. Añado validación. Comprobamos el input del captcha no esté vacío y que además coincida con su valor
 		// utilizando para ello el wp_verify_nonce que nos proporciona WP
 		add_filter('registration_errors', function ($errors) {
-			$captcha = $_POST ['cap'];
-			if (empty(trim($captcha))) {
-				$errors->add('first_name_error', '<strong>ERROR:</strong> Por favor, introduce el captcha.');
-			} else if (!wp_verify_nonce($captcha, $_SESSION ['captcha_action'])) {
+			if (!wp_verify_nonce($_POST ['cap'], $_SESSION ['captcha_action'])) {
 				$errors->add('first_name_error', '<strong>ERROR:</strong> Captcha incorrecto.');
 			}
 			return $errors;
