79aeb92da4978d5f7ce15489a6a24fd3ec878d92
Chemaclass <chemaclass@outlook.es>
2014-10-09 13:50:41 +0200

    Traducir al inglés 'nueva_web_nuevos_objetivos'

--- mvc/i18n/en/tutorial.php
+++ mvc/i18n/en/tutorial.php
@@ -3,9 +3,35 @@ return [
 	'aceptas_terminos_condiciones' => 'If you want to create your own content on the
 			NuevaMetal web app you must agree to the <a href="/legal" target="_blank">terms and conditions</a>
 			of use',
+	'a_un_artista_de_verdad' => "A true artist, don't hurts him that people download his albums, if not they aren't listened",
 
 	'haz_que_tu_banda' => 'Make your band to appear in our sections',
 
+	'publica' => '<h3 class="header titular"><a href="/tutorial">Post</a></h3>
+		<span class="visible-lg col-xs-9 col-xs-push-3 tus-propias-entradas">
+			<i class="fa fa-check fa-2x"></i> Your owns posts
+		</span>
+		<span class="hidden-lg col-xs-12">
+			<i class="fa fa-check"></i> Your owns posts
+		</span>
+		<span class="col-xs-12 col-sm-12 col-lg-6">
+			<i class="fa fa-check"></i> Your music videos
+		</span>
+		<span class="col-xs-12 col-sm-12 col-lg-6">
+			<i class="fa fa-check"></i> News about your band
+		</span>
+		<span class="col-xs-12 col-sm-12 col-lg-6">
+			<i class="fa fa-check"></i>  Concert of your band
+		</span>
+		<span class="col-xs-12 col-sm-12 col-lg-6">
+			<i class="fa fa-check"></i>  Your critics & chronics
+		</span>',
 	'publica_tus_entradas' => 'Publish your posts',
-	'pulsa_aqui_para_ser_colaborador' => 'Click here to become a contributor'
+	'pulsa_aqui_para_ser_colaborador' => 'Click here to become a contributor',
+
+	'nueva_web_nuevos_objetivos' => ' <h3 class="titular">New website, new goals</h3>
+		<span class="col-xs-12"><i class="fa fa-quote-left"></i> New design oriented to bands to post their own discography and become known.
+		</span>
+		<span class="col-xs-12"><i class="fa fa-quote-left"></i> We are working for a better coverage to your posts and news.
+		</span>'
 ];
--- mvc/i18n/es/tutorial.php
+++ mvc/i18n/es/tutorial.php
@@ -3,9 +3,38 @@ return [
 	'aceptas_terminos_condiciones' => 'Si solicitas crear tu contenido en la web aceptas
 			los <a href="/legal" target="_blank">términos
 			y condiciones</a> de uso de la aplicación web NuevaMetal',
+	'a_un_artista_de_verdad' => 'A un artista de verdad, lo que le duele no es que descarguen sus álbumes,
+			sino que no sean escuchados',
 
 	'haz_que_tu_banda' => 'Haz que tu banda aparezca en nuestras secciones',
 
+	'publica' => '<h3 class="header titular"><a href="/tutorial">Publica</a></h3>
+		<span class="visible-lg col-xs-9 col-xs-push-3 tus-propias-entradas">
+			<i class="fa fa-check fa-2x"></i> Tus propias entradas
+		</span>
+		<span class="hidden-lg col-xs-12">
+			<i class="fa fa-check"></i> Tus propias entradas
+		</span>
+		<span class="col-xs-12 col-sm-12 col-lg-6">
+			<i class="fa fa-check"></i> Los videoclips de tu banda
+		</span>
+		<span class="col-xs-12 col-sm-12 col-lg-6">
+			<i class="fa fa-check"></i> Las noticias de tu banda
+		</span>
+		<span class="col-xs-12 col-sm-12 col-lg-6">
+			<i class="fa fa-check"></i>  Los conciertos de tu banda
+		</span>
+		<span class="col-xs-12 col-sm-12 col-lg-6">
+			<i class="fa fa-check"></i>  Tus críticas y/o crónicas
+		</span>',
 	'publica_tus_entradas' => 'Publica tus entradas',
-	'pulsa_aqui_para_ser_colaborador' => 'Pulsa aquí para convertirte en colaborador'
+	'pulsa_aqui_para_ser_colaborador' => 'Pulsa aquí para convertirte en colaborador',
+
+	'nueva_web_nuevos_objetivos' => ' <h3 class="titular">Nueva web, nuevos objetivos</h3>
+		<span class="col-xs-12"><i class="fa fa-quote-left"></i> Un diseño más sencillo para que las bandas
+			puedan publicar su discografía y darse a conocer.
+		</span>
+		<span class="col-xs-12"><i class="fa fa-quote-left"></i> Trabajamos para darle la máxima cobertura
+			a tus entradas.
+		</span>'
 ];
