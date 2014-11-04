ff1758f03c46d3df78814acb3cfddfaa96c5eb77
Chemaclass <chemaclass@outlook.es>
2014-11-04 19:39:04 +0100

    bugfixed: añado i18n a los emails de recuperar pass

--- mvc/i18n/en/emails.php
+++ mvc/i18n/en/emails.php
@@ -6,13 +6,6 @@ return [
 		<b>Pass temp: </b>: :user_pass <br>
 		<b>E-mail</b>: :user_email <br>',
 
-	'aviso_admin_password_reset' => '¡Ey bro!,
-		User  en :blogname <br>
-		<b>Username: </b>: :user_login <br>
-		<b>Pass temp: </b>: :user_pass <br>
-		<b>E-mail</b>: :user_email <br>
-		<b>Key URL: </b>: <a href=" :user_key_url " target="_blank"> :user_key_url </a> <br>',
-
 	'nuevo_user' => 'Hi bro,
 		<br>
 		Welcome to NuevaMetal! Heres how to
@@ -23,13 +16,22 @@ return [
 		If you have any problems, please contact me at :admin_email <br>
 		See you!',
 
-	'password_reset' => 'Hi man,
-		<br>
-			Here you have your new password. Don\'t forget that you can do login
-			<a href="http://nuevametal.com/wp-login.php">here</a>:<br>
-		<b>Nombre de usuario</b>: :user_login <br>
-		<b>Contraseña</b>: :user_pass <br>
-		<br>
-		If you have any problems, please contact me at :admin_email <br>
-		See you!'
+	'password_reset' => '
+		<h2>Requested a password change in <a href=" :blogurl " target="_blank"> :blogname </a></h2>
+		<b>Username:</b> :user_login <br>
+		<b>E-mail:</b> :user_email <br>
+		<b>Request new password</b>:<br>
+		<a href=" :user_key_url " target="_blank"> :user_key_url </a> <br><br>
+		<p>You have requested a password change. Enter this link and it will send an email with a new randomly
+		generated password. Then remember to change it from your user control panel.</p>
+		<small>If you didn\'t request any changes you can skip this mail.</small>',
+
+	'password_reset_2' => '
+		<h2>Retrieving your password in <a href=" :blogurl " target="_blank"> :blogname </a></h2>
+		<b>Username:</b> :user_login <br>
+		<b>E-mail:</b> :user_email <br>
+		<b>Password:</b> :user_pass <br><br>
+		<p>Here you have the new password. Remember to change it from your user control panel. <br>
+		If you have any problems, please contact me :admin_email <br>
+		See you!</p>'
 ];
