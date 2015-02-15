87d1971972147b5811fcb11c75c8d4e3ede3ced4
Chemaclass <chemaclass@outlook.es>
2015-02-15 14:07:21 +0100

    Add ficheros i18n de partners

---
+++ mvc/i18n/en/partners.php
@@ -0,0 +1,11 @@
+<?php
+return [
+
+	'escribenos' => 'Write us',
+
+	'producción_audiovisual' => 'Audiovisual Production',
+
+	'quieres_ser_tu' => 'Want to be you?',
+
+	'webs_amigas_y_partners' => 'Our friends and partners',
+];
---
+++ mvc/i18n/es/partners.php
@@ -0,0 +1,11 @@
+<?php
+return [
+
+	'escribenos' => 'Escríbenos',
+
+	'producción_audiovisual' => 'Producción Audiovisual',
+
+	'quieres_ser_tu' => '¿Quiéres ser tú?',
+
+	'webs_amigas_y_partners' => 'Webs amigas y partners',
+];
